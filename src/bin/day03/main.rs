b7ec9315c5abb43ab6ced9c3b1b1cb6c38901e6c
Andreas Liljeqvist <bonega@gmail.com>
2021-02-28 22:39:02 +0100

    Implement simpler day3

--- src/bin/day03/main.rs
+++ src/bin/day03/main.rs
@@ -1,45 +1,15 @@
 const WIDTH: usize = 31;
 
-#[derive(Copy, Clone, Debug)]
-enum Square {
-    Open,
-    Tree,
-}
-
-struct Map {
-    buffer: Vec<Square>
-}
-
-impl From<&[u8]> for Map {
-    fn from(bytes: &[u8]) -> Self {
-        let res: Vec<_> = bytes.iter()
-            .filter(|x| **x != 10)
-            .map(|x| match x {
-            b'.' => Square::Open,
-            b'#' => Square::Tree,
-            _ => unreachable!(),
-        }).collect();
-        Map { buffer: res }
-    }
-}
-
-impl Map {
-    fn get(&self, x: usize, y: usize) -> Option<Square> {
-        let index = x % WIDTH + y * WIDTH;
-        if index < self.buffer.len() {
-            Some(self.buffer[index])
-        } else { None }
-    }
-}
-
-fn get_trees(map: &Map, right: usize, down: usize) -> usize {
-    let mut x: usize = right;
-    let mut y: usize = down;
-    let mut trees: usize = 0;
+fn get_trees(map: &[u8], right: usize, down: usize) -> usize {
+    let mut x = right;
+    let mut y = down;
+    let mut trees = 0;
     loop {
-        match map.get(x, y) {
-            Some(Square::Tree) => { trees += 1 }
-            Some(Square::Open) => {}
+        let index = x % WIDTH + y * (WIDTH + 1);
+        match map.get(index) {
+            Some(b'#') => { trees += 1 }
+            Some(b'.') => {}
+            Some(c) => unreachable!("{}", c),
             None => { break trees; }
         }
         x += right;
@@ -49,12 +19,11 @@ fn get_trees(map: &Map, right: usize, down: usize) -> usize {
 
 fn main() {
     let buffer = include_bytes!("input.txt");
-    let map = Map::from(&buffer[..]);
-    let walk1 = get_trees(&map, 1, 1);
-    let walk2 = get_trees(&map, 3, 1);
-    let walk3 = get_trees(&map, 5, 1);
-    let walk4 = get_trees(&map, 7, 1);
-    let walk5 = get_trees(&map, 1, 2);
+    let walk1 = get_trees(&buffer[..], 1, 1);
+    let walk2 = get_trees(&buffer[..], 3, 1);
+    let walk3 = get_trees(&buffer[..], 5, 1);
+    let walk4 = get_trees(&buffer[..], 7, 1);
+    let walk5 = get_trees(&buffer[..], 1, 2);
     println!("Problem1: {}", walk2);
     println!("Problem2: {}", walk1 * walk2 * walk3 * walk4 * walk5);
 }
