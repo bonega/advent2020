da5244b25cbd62ce0b8d8d6c2e268814670247e5
Andreas Liljeqvist <bonega@gmail.com>
2021-03-25 13:49:40 +0100

    mep

--- src/bin/day01/main.rs
+++ src/bin/day01/main.rs
@@ -1,12 +1,12 @@
 fn main() {
     let s = include_str!("input.txt");
-    let numbers: Vec<u32> = s.lines().map(|s| s.parse().unwrap()).collect();
+    let numbers: Vec<_> = s.lines().map(|s| s.parse().unwrap()).collect();
 
     println!("Problem1 {}", problem1(&numbers));
     println!("Problem2 {}", problem2(&numbers));
 }
 
-fn problem1(numbers: &[u32]) -> u32 {
+fn problem1(numbers: &[usize]) -> usize {
     for (i, a) in numbers.iter().enumerate() {
         for b in &numbers[i + 1..] {
             if a + b == 2020 {
@@ -18,7 +18,7 @@ fn problem1(numbers: &[u32]) -> u32 {
 }
 
 
-fn problem2(numbers: &[u32]) -> u32 {
+fn problem2(numbers: &[usize]) -> usize {
     for (i, a) in numbers.iter().enumerate() {
         for (j, b) in numbers[i + 1..].iter().enumerate() {
             for c in &numbers[i + j + 1..] {
