ebf0e3c763bbe35ed1cd121baed1c1f04a15e1f7
Andreas Liljeqvist <bonega@gmail.com>
2021-03-25 11:49:19 +0100

    Simplify day1

--- src/bin/day01/main.rs
+++ src/bin/day01/main.rs
@@ -1,16 +1,16 @@
 fn main() {
     let s = include_str!("input.txt");
-    let numbers:Vec<u32> = s.lines().map(|s| s.parse().unwrap()).collect();
+    let numbers: Vec<u32> = s.lines().map(|s| s.parse().unwrap()).collect();
 
     println!("Problem1 {}", problem1(&numbers));
     println!("Problem2 {}", problem2(&numbers));
 }
 
 fn problem1(numbers: &[u32]) -> u32 {
-    for (i, v) in numbers.iter().enumerate() {
-        for v2 in &numbers[i..] {
-            if v + v2 == 2020 {
-                return v * v2;
+    for (i, a) in numbers.iter().enumerate() {
+        for b in &numbers[i..] {
+            if a + b == 2020 {
+                return a * b;
             }
         }
     };
@@ -19,11 +19,11 @@ fn problem1(numbers: &[u32]) -> u32 {
 
 
 fn problem2(numbers: &[u32]) -> u32 {
-    for (i, v) in numbers.iter().enumerate() {
-        for (j, v2) in numbers[i..].iter().enumerate() {
-            for v3 in &numbers[i + j..] {
-                if v + v2 + v3 == 2020 {
-                    return v * v2 * v3;
+    for (i, a) in numbers.iter().enumerate() {
+        for (j, b) in numbers[i..].iter().enumerate() {
+            for c in &numbers[i + j..] {
+                if a + b + c == 2020 {
+                    return a * b * c;
                 }
             }
         }
